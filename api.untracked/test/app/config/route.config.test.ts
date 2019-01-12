9018a8d969a43ec70dc718fb20bcf7dff37f13ee
Gus Crawford <crawford.gus@gmail.com>
2019-01-12 11:37:28 -0500

    remove

---
+++ api.untracked/test/app/config/route.config.test.ts
@@ -0,0 +1,51 @@
+import * as Request from 'request-promise-native';
+import { UNAUTHORIZED } from 'http-status-codes';
+import 'mocha';
+import { equal } from 'assert';
+import { MockExpress, MockApp } from '../../mocks';
+process.env.PORT = '3000';
+// require('../../../src/www');    // Start the server
+import { RouteFactory } from '../../../src/app/config/route.config';
+RouteFactory.expressInstance = MockExpress.instance;
+
+process.env.DEFAULT_SESSION_HEADER='x';
+import { config as session } from '../../../src/app/config/session-jwt.config';
+session(MockApp.instance,{});
+import { route as usersRoute } from '../../../untracked-src/routes/users';
+//const USERS_ROUTE = 'users';
+describe(`⚙️  User Route config`, function (){
+    //console.info(routes);
+    it(` 👤  users route is configured as per schema`, function () {
+        equal(
+            usersRoute.router.get.calledWith(
+                '/',
+                usersRoute.sessionProtection
+                    && usersRoute.sessionProtection.protects('/users'),
+                usersRoute.routeMap['/'].get[`$handle_get_/users/`]
+            )
+            , true
+        );
+    })
+    // it(`http://localhost:${process.env.PORT}/users returns "unauthorized" (${UNAUTHORIZED})`, async function() {
+
+    //     var result;
+    //     try {
+    //         result = await Request.get(`http://localhost:${process.env.PORT}/users`);
+    //     }
+    //     catch(e) {
+    //         equal(e.statusCode, UNAUTHORIZED)
+    //     }
+    //     return result;
+    // });
+    // it(`http://localhost:${process.env.PORT}/users/x returns "unauthorized" (${UNAUTHORIZED})`, async function () {
+    //     var result;
+    //     try {
+    //         result = await Request.get(`http://localhost:${process.env.PORT}/users/x`);
+    //     }
+    //     catch(e) {
+    //         equal(e.statusCode, UNAUTHORIZED)
+    //     }
+    //     return result;
+    // });
+});
+//process.exit(0);
