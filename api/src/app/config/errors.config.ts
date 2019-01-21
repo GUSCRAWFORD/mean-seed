1a60483b834eeb8632f7313ce3c44fe64488ca8f
Gus Crawford <crawford.gus@gmail.com>
2019-01-21 08:55:17 -0500

    error should match request type if no accept was set

--- api/src/app/config/errors.config.ts
+++ api/src/app/config/errors.config.ts
@@ -4,10 +4,9 @@ export function config(app:Application) {
     app.use(function(err:any, req:Request, res:Response, next:(args?:any)=>any) {
       const verboseStackTrace = req.app.get('env') === 'development';
       if ((req as any).headersSent) return next(err);
-
-      // render the error page
+      var type = req && req.headers && (req.headers.accept || req.headers["content-type"] || '').split(/\s*;\s*/).pop();
       res.status(err.status || 500);
-      switch (req.headers.accept) {
+      switch (type) {
         case 'application/json':
           applicationJson(res, err, verboseStackTrace);
           break;
@@ -15,7 +14,8 @@ export function config(app:Application) {
           // set locals, only providing error in development
           res.locals.message = err.message;
           res.locals.error = verboseStackTrace ? err : {};
-          res.locals.title = `${err.message} (${err.status})`
+          res.locals.title = `${err.message} (${err.status})`;
+        // render the error page
           res.render('error');
       }
     });
