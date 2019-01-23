3c107355814cf0dab7829f54e769f2636f7bdce4
Gus Crawford <crawford.gus@gmail.com>
2019-01-23 09:07:44 -0500

    use express-session

--- api/src/app/config/session.config.ts
+++ api/src/app/config/session.config.ts
@@ -1,6 +1,7 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import * as session from 'express-session';
-import { DEBUG } from "../services";
+import { Passport, Authenticator } from 'passport';
+import { DEBUG, Sequence } from '../services';
 const DEBUG_TOPIC='session';
 export const DEFAULT_SESSION_SECRET = process.env.SESSION_SECRET || 'hardcoded-secret';
 export const DEFAULT_SESSION_HEADER = process.env.SESSION_HEADER || 'x-token';
@@ -25,6 +26,13 @@ export class Protection {
         return protectionKey?this.paths[protectionKey]:(q:Request,s:Response,x:(q:Request,s:Response,err?:any)=>any)=>x(q,s,null);
     }
 }
+const SESSION_CONFIG = {
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET||DEFAULT_SESSION_SECRET,
+    signed: true
+};
+const PASSPORT = new Passport();
 /**
  * 
  * @param app
@@ -33,11 +41,21 @@ export function config(
     app:any,
     options?:SessionConfigOptions
  ) {
-    if (DEBUG(DEBUG_TOPIC)) {
-        var sessionConfigSe
-    }
     options = Object.assign(new SessionConfigOptions(), options);
+
+    if (DEBUG(DEBUG_TOPIC))  console.info(`⚙️  👤  ${Sequence.named.configSequence.label}  Use Express-Session`);
+    app.use(session(SESSION_CONFIG));
+    
     if (options.sessionConfigs) options.sessionConfigs.forEach(
-        sessionConfig=>sessionConfig()
+        sessionConfig=>{
+            if (DEBUG(DEBUG_TOPIC))  console.info(`⚙️  👤  ${Sequence.named.configSequence.label}  Sub-session Config`);
+            sessionConfig(PASSPORT /* singleton-middleware injections */);
+        }
     )
+
+    if (DEBUG(DEBUG_TOPIC))  console.info(`⚙️  👤  ${Sequence.named.configSequence.label}  Initialize Passport`);
+    app.use(PASSPORT.initialize());
+
+    if (DEBUG(DEBUG_TOPIC))  console.info(`⚙️  👤  ${Sequence.named.configSequence.label}  Use Passport-Session`);
+    app.use(PASSPORT.initialize());
 }
